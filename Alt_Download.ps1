c6a923e2124661eb16e8ab3c7e8264ccc443c767
Michael Nieto <mikensec@users.noreply.github.com>
2022-03-22 09:55:15 -0500

    Create Alt_Download.ps1

---
+++ Alt_Download.ps1
@@ -0,0 +1,6 @@
+﻿#Invoke-WebRequest -Uri "https://api.agent.alienvault.cloud/osquery-api/eu-central-1/bootstrap?flavor=powershell" -OutFile "av-installer.ps1"
+
+#.\av-installer install_agent -controlnodeid 032da443-7937-4074-9731-681d542919a5
+
+
+
